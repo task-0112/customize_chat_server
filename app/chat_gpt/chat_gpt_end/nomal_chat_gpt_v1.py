ce84dd569ecc91f0a275e04934ed619369853ab0
浅尾 刀麻 <touma.asao@arsaga.jp>
2024-05-11 12:31:42 +0900

    不必要なクラス削除した

--- app/chat_gpt/chat_gpt_end/nomal_chat_gpt_v1.py
+++ app/chat_gpt/chat_gpt_end/nomal_chat_gpt_v1.py
@@ -2,24 +2,19 @@
 ChatGpt通常回答のエンドポイント
 """
 
-from typing import Dict
 from fastapi import APIRouter
-from pydantic import BaseModel
 from app.chat_gpt.chat_gpt_answer.nomal_chat_gpt_answer import GptNomalStreemResponse
 from sse_starlette import EventSourceResponse
+from app.schemas.chat import ChatRequest
 
 router = APIRouter()
 
 
-class AskRequest(BaseModel):
-    query: str
-
-
 @router.post("/nomal/chat/v1")
-async def ask_stream(request_body: Dict) -> EventSourceResponse:
-    role = request_body.get("role")
-    content = request_body.get("content")
-    model_type = request_body.get("modelType")
+async def ask_stream(request_body: ChatRequest) -> EventSourceResponse:
+    role = request_body.role
+    content = request_body.content
+    model_type = request_body.modelType
     gpt_normal_stream_response = GptNomalStreemResponse(model_type)
     return EventSourceResponse(
         gpt_normal_stream_response._generate_response(role, content)
